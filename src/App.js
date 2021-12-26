0b6c18d60a8e8731d86b44272db35766a21aced7
talin2412 <talin2412@gmail.com>
2021-12-26 03:30:33 -0500

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes} from 'react-router-dom';
+import {Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import News from "./components/News";
+import CryptoDetails from "./components/CryptoDetails";
+import Exchanges from "./components/Exchanges";
+
+import './App.css'
+
+
+const App = () => {
+        return (
+                <div className="app">
+                    <div className="navbar">
+                        <Navbar />
+                    </div>
+                    <div className="main">
+                        <Layout>
+                            <div className="routes">
+                                    <Routes>
+                                        <Route exact path="/" element={<HomePage />} />
+                                        <Route exact path="/exchanges" element={<Exchanges />} />
+                                        <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                                        <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+                                        <Route exact path="/news" element={<News />} />
+                                    </Routes>
+                            </div>
+                        </Layout>
+                    </div>
+                    {/* <div className="footer">
+                    </div> */}
+                </div>
+        )
 }
 
-export default App;
+export default App
